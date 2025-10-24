8427d5fe8eda9b811565f3e70e6ce8cabf77275d
ArturInspector <bikk_artur@mail.ru>
2025-10-24 21:31:17 +0600

    [BACK] feat: Verificationsbt - supportinterf, mint function

--- contracts/core/VerificationSBT.sol
+++ contracts/core/VerificationSBT.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -22,6 +21,35 @@ contract VerificationSBT is ERC721, AccessControl {
     constructor(address minter) ERC721("VerificationSBT", "vSBT") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, minter);
+    }
+
+    function mint(address to, uint8 source, bytes32 uniqueId) external onlyRole(MINTER_ROLE) returns (uint256) {
+        uint256 tokenId = _tokenIdCounter++;
+        
+        metadata[tokenId] = SBTMetadata({
+            source: source,
+            uniqueId: uniqueId,
+            timestamp: block.timestamp
+        });
+        
+        _safeMint(to, tokenId);
+        return tokenId;
+    }
+    
+    function totalSupply() external view returns (uint256) {
+        return _tokenIdCounter;
+    }
+    // sbt block 
+    function _update(address to, uint256 tokenId, address auth) internal override returns (address) {
+        address from = _ownerOf(tokenId);
+        if (from != address(0) && to != address(0)) {
+            revert TransferNotAllowed();
+        }
+        return super._update(to, tokenId, auth);
+    }
+    
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
     
 }
